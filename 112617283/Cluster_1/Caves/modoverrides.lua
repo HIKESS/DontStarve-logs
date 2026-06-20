ca7ee74e6fedb4379dac5a58c2105e8ceb3a4bb7
Fabio <jamesonsuarezctid@outlook.com>
2026-06-20 14:34:57 -0300

    logs

---
+++ 112617283/Cluster_1/Caves/modoverrides.lua
@@ -0,0 +1,19 @@
+return {
+  WagstaffPlus={
+    configuration_options={
+      debug=true,
+      enableblur=true,
+      flickerthreshold=0.75,
+      gogglesrestricted=false,
+      language="auto",
+      mysterychance=0.03,
+      mysterymidchance=0.7,
+      mysterynilchance=0.4,
+      preset="NONE",
+      telebrellarandom=true,
+      telebrellaspelltype="item",
+      weakstomachpain=-3 
+    },
+    enabled=true 
+  } 
+}
